087251069856c8e5c34b9951aed41254b89bb139
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-10-01 15:33:12 +0100

    First Commit with rails new app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookStore::Application.config.secret_token = '3e600c80dbdb6cf005b6fbbfc9a512b30988fc76022907b4573564595b10f815a1ed2de0c9891de52af0edae1389b940beec54637cec361fbea56ac09bc90a88'
